2e560d68e1b7ebcb8ba6ffe59ce4888cade85abe
Akanksha <reactdevpzealth@gmail.com>
2025-06-18 19:51:17 +0530

    footer ui issue fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
     <footer className="bg-gray-900 dark:text-white px-6 py-20  lg:px-32 shadow-md ">
       <div className="max-w-screen-xl mx-auto flex flex-col gap-8 lg:flex-row justify-between items-center">
         {/* Logo or Name */}
-        <div className="text-3xl font-bold cursor-pointer text-center lg:text-left">
+        <div className="text-3xl font-bold cursor-pointer text-center lg:text-left text-white">
           Akansha.
         </div>
 
@@ -24,7 +24,7 @@ const Footer = () => {
             href="https://www.linkedin.com/in/akansha1719/"
             target="_blank"
             rel="noopener noreferrer"
-            className="hover:text-brightColor transition-all"
+            className="hover:text-brightColor transition-all text-white"
           >
             <FaLinkedin size={30} />
           </a>
@@ -32,7 +32,7 @@ const Footer = () => {
             href="mailto:gautamakansha312@gmail.com"
             target="_blank"
             rel="noopener noreferrer"
-            className="hover:text-brightColor transition-all"
+            className="hover:text-brightColor transition-all text-white"
           >
             <BiLogoGmail size={30} />
           </a>
@@ -40,14 +40,14 @@ const Footer = () => {
             href="https://www.hackerrank.com/profile/gautamakansha312"
             target="_blank"
             rel="noopener noreferrer"
-            className="hover:text-brightColor transition-all"
+            className="hover:text-brightColor transition-all text-white"
           >
             <FaHackerrank size={30} />
           </a>
           <a
             target="_blank"
             rel="noopener noreferrer"
-            className="hover:text-brightColor transition-all"
+            className="hover:text-brightColor transition-all text-white"
           >
             <BsInstagram size={30} />
           </a>
@@ -55,7 +55,7 @@ const Footer = () => {
       </div>
 
       {/* Footer Bottom Text */}
-      <div className="mt-10 text-center text-sm text-gray-600 dark:text-gray-400">
+      <div className="mt-10 text-center text-sm text-gray-600 dark:text-gray-200">
         &copy; {new Date().getFullYear()} Akansha. All rights reserved.
       </div>
     </footer>
